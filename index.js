4be685d0f839e4ddfdffc12c6a75317b122cc5e7
kamontat <kamontat_c@hotmail.com>
2017-06-25 16:54:42 +0700

    try

--- index.js
+++ index.js
@@ -2,18 +2,17 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
+const db = require('./src/db')
+
 app.use(express.static('public'))
 
 // app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-    console.log();
-})
-
-app.post('/createUser', (req, res) => {
-    console.log("creating user")
+    res.json(db);
     res.sendStatus(200)
 })
+
 app.listen(8080, () => {
     console.log('Server running on http://localhost:8080')
 })
