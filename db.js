77a97e36757f22bbac78bf0c25e2d467031eadaa
kamontat <kamontat_c@hotmail.com>
2017-06-25 16:07:44 +0700

    add db management

---
+++ db.js
@@ -0,0 +1,12 @@
+const config = require('./db_config');
+const Client = require('mariasql');
+
+var cli = new Client(config);
+
+cli.query('SHOW DATABASES', function(err, rows) {
+  if (err)
+    throw err;
+  console.dir(rows);
+});
+
+cli.end();
