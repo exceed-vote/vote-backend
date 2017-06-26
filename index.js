b046b4b9de82c86ac5ecec582803966f7fb63e43
kamontat <kamontat_c@hotmail.com>
2017-06-26 15:16:18 +0700

    start work with api

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const resTime = require('response-time')
+const morgan = require('morgan')
+const compression = require('compression')
 const app = express()
 const db = require('./src/db')
 
+var utf8 = require('utf8')
+
 app.use(express.static('public'))
+app.use(bodyParser.json())
+morgan(':method :url :status :res[content-length] - :response-time ms')
+app.use(resTime())
+app.use(compression())
 
-// app.use(bodyParser.json())
-
-app.get('/', (req, res) => {
-    db.show().then(function(resolve, rej) {
-        console.log(resolve);
-        res.sendStatus(200);
-    });
+app.get('/user', (req, res) => {
+    db.user().then(function(resolve) {
+        // var name = resolve[0].name
+        res.status(200).json(resolve);
+    }).catch(function (rej) {
+        console.error(rej)
+    })
 })
+
+app.get('/insert', (req, res) => {
+    var data = {
+        code: 2,
+        name: "ทำไม",
+        description: "ไม่มี",
+        picture: "asdf"
+    };
+    db.insert(data).then(function(resolve) {
+        console.log(resolve);
+    }).catch(function (rej) {
+        console.error(rej);
+    });
+});
 
 app.listen(8080, () => {
     console.log('Server running on http://localhost:8080')
