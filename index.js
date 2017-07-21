139ed40b40fb0e1d560a119fa925a7151a76dc31
babestvl <babestvl@gmail.com>
2017-07-21 09:39:01 +0700

    Bug fix auth

--- index.js
+++ index.js
@@ -17,6 +17,26 @@ morgan(':method :url :status :res[content-length] - :response-time ms')
 app.use(resTime())
 app.use(compression())
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.get('/', (req, res) => {
     logutil.logger('history', "0000000000", req.ip).info("go to root!")
     res.status(200).send({
@@ -83,6 +103,7 @@ app.post('/insert', (req, res) => {
 // })
 
 app.post('/login', (req, res) => {
+    logutil.logger('common', "0000000000", req.ip).info(req.body)   
     db.auth(req.body).then((result) => {
         let num = Number.parseInt(result.info.numRows)
         if (num !== 1) {
